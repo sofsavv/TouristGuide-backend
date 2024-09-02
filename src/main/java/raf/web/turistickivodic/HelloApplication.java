08180df198ebaed947f964904aa85564cb146663
Sofija Savic <ssavic14322rn@raf.rs>
2024-09-02 16:41:24 +0200

    backend - gotov

--- src/main/java/raf/web/turistickivodic/HelloApplication.java
+++ src/main/java/raf/web/turistickivodic/HelloApplication.java
@@ -3,12 +3,17 @@ package raf.web.turistickivodic;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
+import raf.web.turistickivodic.repositories.activity.ActivityRepository;
+import raf.web.turistickivodic.repositories.activity.MySqlActivityRepository;
+import raf.web.turistickivodic.repositories.article.ArticleRepository;
+import raf.web.turistickivodic.repositories.article.MySqlArticleRepository;
+import raf.web.turistickivodic.repositories.comment.CommentRepository;
+import raf.web.turistickivodic.repositories.comment.MySqlCommentRepository;
 import raf.web.turistickivodic.repositories.destination.DestinationRepository;
 import raf.web.turistickivodic.repositories.destination.MySqlDestRepository;
 import raf.web.turistickivodic.repositories.user.MySqlUserRepository;
 import raf.web.turistickivodic.repositories.user.UserRepository;
-import raf.web.turistickivodic.services.DestinationService;
-import raf.web.turistickivodic.services.UserService;
+import raf.web.turistickivodic.services.*;
 
 import javax.inject.Singleton;
 import javax.ws.rs.ApplicationPath;
@@ -27,9 +32,14 @@ public class HelloApplication extends ResourceConfig {
             protected void configure() {
                 this.bind(MySqlUserRepository.class).to(UserRepository.class).in(Singleton.class);
                 this.bind(MySqlDestRepository.class).to(DestinationRepository.class).in(Singleton.class);
+                this.bind(MySqlCommentRepository.class).to(CommentRepository.class).in(Singleton.class);
+                this.bind(MySqlArticleRepository.class).to(ArticleRepository.class).in(Singleton.class);
+                this.bind(MySqlActivityRepository.class).to(ActivityRepository.class).in(Singleton.class);
                 this.bindAsContract(UserService.class);
                 this.bindAsContract(DestinationService.class);
-
+                this.bindAsContract(CommentService.class);
+                this.bindAsContract(ArticleService.class);
+                this.bindAsContract(ActivityService.class);
             }
         };
         register(binder);
